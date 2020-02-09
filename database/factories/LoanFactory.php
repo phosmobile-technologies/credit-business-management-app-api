1801091598c5ec6b05ea6a89e291a0e9e668d2af
oluwablin <oyelekeseun@outlook.com>
2020-02-09 18:14:36 +0100

    updated database for loan bugs

--- database/factories/LoanFactory.php
+++ database/factories/LoanFactory.php
@@ -3,22 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model;
-use App\Models\Enums\DisbursementStatus;
 use App\Models\Enums\LoanApplicationStatus;
-use App\Models\Enums\LoanConditionStatus;
-use App\Models\Enums\LoanDefaultStatus;
 use App\Models\Enums\LoanRepaymentFrequency;
 use App\Models\Loan;
 use Faker\Generator as Faker;
 
 $factory->define(Loan::class, function (Faker $faker) {
     $loanFrequencies = [LoanRepaymentFrequency::WEEKLY, LoanRepaymentFrequency::MONTHLY];
-    $loanConditionStatuses = [
-        LoanConditionStatus::NONPERFORMING,
-        LoanConditionStatus::COMPLETED,
-        LoanConditionStatus::INACTIVE,
-        LoanConditionStatus::ACTIVE
-    ];
+    
     $loanAmount =  $faker->randomFloat(2, 10000, 10000000);
     $amountPaid = $faker->randomFloat(2, 10000, 5000000);
 
@@ -27,59 +19,48 @@ $factory->define(Loan::class, function (Faker $faker) {
         'loan_purpose' => $faker->realText(),
         'loan_repayment_source' => $faker->realText(),
         'loan_amount' => $loanAmount,
-        'loan_balance' => $loanAmount -  $amountPaid,
-        'disbursement_date' => $faker->date(),
-        'disbursement_status' => DisbursementStatus::DISBURSED,
-        'amount_disbursed' => $amountPaid,
         'interest_rate' => $faker->randomFloat(2, 10, 30),
         'loan_repayment_frequency' => $loanFrequencies[array_rand($loanFrequencies)],
-        'next_due_payment' => $faker->date(),
-        'due_date' => $faker->date(),
         'service_charge' => $faker->randomFloat(2, 1000, 10000),
-        'default_amount' => 1000,
-        'tenure' => $faker->numberBetween(1, 24),
-        'application_status' => LoanApplicationStatus::PENDING,
-        'loan_condition_status' => $loanConditionStatuses[array_rand($loanConditionStatuses)],
-        'loan_default_status' => LoanDefaultStatus::NOT_DEFAULTING,
-        'num_of_default_days' => null
+        'tenure' => $faker->numberBetween(1, 24)
     ];
 });
 
-/**
- * Factory state for a loan that is defaulting
- */
-$factory->state(Loan::class, 'defaulting_loan', function ($faker) {
-    return [
-        'loan_default_status' => LoanDefaultStatus::DEFAULTING,
-        'num_of_default_days' => $faker->numberBetween(1, 30)
-    ];
-});
+// /**
+//  * Factory state for a loan that is defaulting
+//  */
+// $factory->state(Loan::class, 'defaulting_loan', function ($faker) {
+//     return [
+//         'loan_default_status' => LoanDefaultStatus::DEFAULTING,
+//         'num_of_default_days' => $faker->numberBetween(1, 30)
+//     ];
+// });
 
-/**
- * Factory state for a loan that is not defaulting
- */
-$factory->state(Loan::class, 'non_defaulting_loan', function ($faker) {
-    return [
-        'loan_default_status' => LoanDefaultStatus::NOT_DEFAULTING,
-        'num_of_default_days' => null
-    ];
-});
+// /**
+//  * Factory state for a loan that is not defaulting
+//  */
+// $factory->state(Loan::class, 'non_defaulting_loan', function ($faker) {
+//     return [
+//         'loan_default_status' => LoanDefaultStatus::NOT_DEFAULTING,
+//         'num_of_default_days' => null
+//     ];
+// });
 
-/**
- * Factory state for a loan that has been disbursed
- */
-$factory->state(Loan::class, 'disbursed_loan', function ($faker) {
-    return [
-        'disbursement_status' => DisbursementStatus::DISBURSED
-    ];
-});
+// /**
+//  * Factory state for a loan that has been disbursed
+//  */
+// $factory->state(Loan::class, 'disbursed_loan', function ($faker) {
+//     return [
+//         'disbursement_status' => DisbursementStatus::DISBURSED
+//     ];
+// });
 
-/**
- * Factory state for a loan that has not been disbursed
- */
-$factory->state(Loan::class, 'not_disbursed_loan', function ($faker) {
-    return [
-        'disbursement_status' => DisbursementStatus::NOT_DISBURSED,
-        'amount_disbursed' => null
-    ];
-});
+// /**
+//  * Factory state for a loan that has not been disbursed
+//  */
+// $factory->state(Loan::class, 'not_disbursed_loan', function ($faker) {
+//     return [
+//         'disbursement_status' => DisbursementStatus::NOT_DISBURSED,
+//         'amount_disbursed' => null
+//     ];
+// });
