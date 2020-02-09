dac0950094a1d51d5343c0dc70bded3226fa77b3
oluwablin <oyelekeseun@outlook.com>
2020-02-09 18:13:40 +0100

    updated Provider for loan activity records

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Events\NewLoanCreated;
 use App\Events\NewUserRegistered;
+use App\Events\NewLoanActivity;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,6 +28,9 @@ class EventServiceProvider extends ServiceProvider
 
         NewLoanCreated::class => [
             "App\Listeners\SendLoanCreatedEmail"
+        ],
+        NewLoanActivity::class => [
+            "App\Listeners\SendLoanActivityEmail"
         ]
     ];
 
