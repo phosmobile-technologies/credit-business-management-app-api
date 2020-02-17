18a5649cf6e5fa841f806ff00a6490f7543adbcf
oluwablin <oyelekeseun@outlook.com>
2020-02-17 15:35:19 +0100

    [feature/50/customer-withdrawal-request] updated database seeder for calling customer withdrawal request table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(LoanTableSeeder::class);
         $this->call(TransactionsTableSeeder::class);
         $this->call(ContributionPlanTableSeeder::class);
+        $this->call(CustomerWithdrawalRequestTableSeeder::class);
     }
 }
