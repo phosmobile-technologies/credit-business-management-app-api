cb217742d6c553aec2552a6e2b27c044949d6245
oluwablin <oyelekeseun@outlook.com>
2020-02-18 12:07:32 +0100

    [feature/50/customer-withdrawal-request] created events

---
+++ app/Events/CustomerwithdrawalRequestDisApprovedByGlobalManager.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\CustomerWithdrawalRequest;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CustomerwithdrawalRequestDisApprovedByGlobalManager
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param CustomerWithdrawalRequest $customerwithdrawalrequest
+     * @param null|string $message
+     */
+    public function __construct(CustomerWithdrawalRequest $customerwithdrawalrequest, ?string $message)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
