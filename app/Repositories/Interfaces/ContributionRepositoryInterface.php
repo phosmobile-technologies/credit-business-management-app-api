b46ede778e2f111c32fafe432868df2ec8912434
oluwablin <oyelekeseun@outlook.com>
2020-02-26 12:04:45 +0100

    [feature/56/customer-wallet] disable withdrawal from contribution

--- app/Repositories/Interfaces/ContributionRepositoryInterface.php
+++ app/Repositories/Interfaces/ContributionRepositoryInterface.php
@@ -45,13 +45,4 @@ interface ContributionRepositoryInterface
      * @return ContributionPlan
      */
     public function addPayment(ContributionPlan $contribution, Transaction $transaction): ContributionPlan;
-
-    /**
-     * Withdraw funds from a contribution plan.
-     *
-     * @param ContributionPlan $contribution
-     * @param Transaction $transaction
-     * @return ContributionPlan
-     */
-    public function withdraw(ContributionPlan $contribution, Transaction $transaction): ContributionPlan;
 }
