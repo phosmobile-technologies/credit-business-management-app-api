2d515ff41b033ced8c1245c0e4e35e4195edde34
Omadoye Abraham <omadoye@trackstreet.com>
2020-07-08 13:42:13 +0100

    Refactored new signup email to display button properly

--- resources/views/email/signup/user.blade.php
+++ resources/views/email/signup/user.blade.php
@@ -1,29 +1,27 @@
 @component('mail::message')
+@component('mail::panel')
+Hello **{{ $user["first_name"] }} {{ $user["last_name"] }},**
 
-    @component('mail::panel')
-        Hello **{{ $user["first_name"] }} {{ $user["last_name"] }},**
+You have successfully been registered on the UMC platform.
 
-        You have successfully been registered on the UMC platform.
+@if($registration_source == \App\Models\enums\RegistrationSource::BACKEND)
+    {
+    Please find your login credentials below:
 
-        @if($registration_source == \App\Models\enums\RegistrationSource::BACKEND)
-            {
-            Please find your login credentials below:
+    * Email - {{ $user['email'] }}
+    * Default Password - {{ $defaultPassword }}
+    }
+@endif
 
-            * Email - {{ $user['email'] }}
-            * Default Password - {{ $defaultPassword }}
-            }
-        @endif
+@component('mail::button', ['url' => $loginUrl])
+    Log in to your account
+@endcomponent
 
-        @component('mail::button', ['url' => $loginUrl])
-            Log in to your account
-        @endcomponent
+If you are not a Springverse customer, please disregard this email.
+please disregard this email.
 
-        If you are not a Springverse customer, please disregard this email.
-        please disregard this email.
+Welcome on board,<br>
+UMC Team
 
-        Welcome on board,<br>
-        UMC Team
-
-    @endcomponent
-
+@endcomponent
 @endcomponent
